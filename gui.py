eb3e37c3fc6becdfd316af12a2ffebcff15f2197
Nandini Malge <nandini.malge2006@gmail.com>
2026-06-17 21:25:15 +0530

    added counter, start and stop function

--- gui.py
+++ gui.py
@@ -21,21 +21,47 @@ count_label.pack()
 key_count = 0
 listener = None
 
+def update_ui(count):
+    root.after(0, lambda: count_label.config(text=f"Total Keystrokes: {count}"))
 
+def on_press(key):
+    write_to_file(key, update_ui)
 
+def start_logging():
 
+    global listener
 
+    status_label.config(
+         text = "Status: Monitoring...",
+         font = ("Consolas", 14),
+         fg = "green",
+         bg="#0D1117"
+    )
 
+    listener = Listener(on_press=on_press)
+    listener.start()
 
+def stop_logging():
 
+    global listener
 
+    if listener:
+        listener.stop()
+        listener = None
 
+    file_path = os.path.abspath("log.txt")
 
-
-
-
-
-
+    status_label.config(
+        text="Status: Stopped",
+        font = ("Consolas", 14),
+        fg = "red",
+        bg="#0D1117")
+    
+    messagebox.showinfo(
+        "Logging Stopped",
+        f"log.txt saved successfully!\n\nPath:\n{file_path}"
+    )
+   
 start_btn = Button(root, text="Start Capture", font=("Consolas", 16, "bold"), fg="black", bg="#39FF14", width=15, command=start_logging)
 start_btn.pack(pady=30)
 
